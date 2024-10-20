06951666a5ea103ed727b05f9134b79f90922587
jelanmathewjames <jelanmathewjames1234@gmail.com>
2024-10-20 14:05:37 +0530

    login api

--- todo/controller/auth.go
+++ todo/controller/auth.go
@@ -2,7 +2,15 @@ package controller
 
 import (
 	"github.com/gofiber/fiber/v2"
+
+	"todo/db"
+	"todo/util"
 )
+
+type RegisterData struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
 
 func Login(c *fiber.Ctx) error {
 	c.SendString("I'm a POST request!")
@@ -10,12 +18,31 @@ func Login(c *fiber.Ctx) error {
 }
 
 func Register(c *fiber.Ctx) error {
-	var formated_data map[string]interface{}
+	var formated_data RegisterData
 
 	if err := c.BodyParser(&formated_data); err != nil {
-		return err
+		return c.JSON(fiber.Map{
+			"message": err,
+		})
+	}
+	email := formated_data.Email
+	password := formated_data.Password
+
+	session, err := db.DBConnection()
+	if err != nil {
+		return c.JSON(fiber.Map{
+			"message": "Failed to connect to database",
+		})
 	}
 
+	hashed_password, err := util.HashPassword(password)
+	if err != nil {
+		return c.JSON(fiber.Map{
+			"message": "Internal Server Error",
+		})
+	}
+
+	session.Create(db.User{Email: email, Password: hashed_password})
 	return c.JSON(fiber.Map{
 		"message": "Registered Successfully",
 	})
