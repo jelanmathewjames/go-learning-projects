1c866af346a98f21878faeaadb444a6b1e4b7a55
jelanmathewjames <jelanmathewjames1234@gmail.com>
2024-10-27 22:09:18 +0530

    feat: add jwt utils

--- todo/go.mod
+++ todo/go.mod
@@ -4,6 +4,7 @@ go 1.23.0
 
 require (
 	github.com/gofiber/fiber/v2 v2.52.5
+	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/google/uuid v1.6.0
 	golang.org/x/crypto v0.14.0
 	gorm.io/driver/sqlite v1.5.6
--- todo/util/utils.go
+++ todo/util/utils.go
@@ -1,6 +1,11 @@
 package util
 
 import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -11,4 +16,52 @@ func HashPassword(password string) (string, error) {
 
 func ComparePassword(password, hash string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+}
+
+func GenerateToken(userId string) (map[string]string, error) {
+	key := []byte(os.Getenv("SECRET_KEY"))
+	access_jwt_instance := jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"user_id": userId,
+			"type":    "access",
+			"exp":     time.Now().Add(time.Hour * 24).Unix(),
+		})
+	access_token, err := access_jwt_instance.SignedString(key)
+	if err != nil {
+		return nil, err
+	}
+	refresh_jwt_instance := jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"user_id": userId,
+			"type":    "refresh",
+			"exp":     time.Now().Add(time.Hour * 24 * 7).Unix(),
+		})
+	refresh_token, err := refresh_jwt_instance.SignedString(key)
+	if err != nil {
+		return nil, err
+	}
+	return map[string]string{
+		"access_token":  access_token,
+		"refresh_token": refresh_token,
+	}, nil
+}
+
+func VerifyToken(tokenString string) (jwt.MapClaims, error) {
+	key := []byte(os.Getenv("SECRET_KEY"))
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return key, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claims, nil
+	} else {
+		return nil, err
+	}
 }
